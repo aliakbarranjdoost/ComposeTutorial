3a394ed97f9a7d05b377d81faa38b9a401c7773d
AliAkbar <aliakbarranjdoost@gmail.com>
2023-08-29 12:19:34 +0330

    Pathway 1 - Activity 1 - Part 2 : Layouts

--- app/src/main/java/dev/aliakbar/composetutorial/MainActivity.kt
+++ app/src/main/java/dev/aliakbar/composetutorial/MainActivity.kt
@@ -3,9 +3,22 @@ package dev.aliakbar.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity()
 {
@@ -14,20 +27,45 @@ class MainActivity : ComponentActivity()
         super.onCreate(savedInstanceState)
         setContent()
         {
-            MessageCard(name = "Android")
+            MessageCard(Message(author = "Android", body = "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String)
+fun MessageCard(message: Message)
 {
-    Text("Hello $name!")
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp))
+    {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column()
+        {
+            Text(message.author)
+            // Add a vertical space between the author and the message
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(message.body)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard()
 {
-    MessageCard(name = "Android")
+    MessageCard(Message(author = "Colleague", body = "Hey, take look at Jetpack Compose, It`s great!"))
 }
