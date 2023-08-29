efdf00d0287cb81b47bb959fe5b78674b558f8e2
AliAkbar <aliakbarranjdoost@gmail.com>
2023-08-29 12:04:47 +0330

    Pathway 1 - Activity 1 - Part 1 : Composable functions

--- app/src/main/java/dev/aliakbar/composetutorial/MainActivity.kt
+++ app/src/main/java/dev/aliakbar/composetutorial/MainActivity.kt
@@ -3,14 +3,9 @@ package dev.aliakbar.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.aliakbar.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity()
 {
@@ -19,7 +14,20 @@ class MainActivity : ComponentActivity()
         super.onCreate(savedInstanceState)
         setContent()
         {
-
+            MessageCard(name = "Android")
         }
     }
+}
+
+@Composable
+fun MessageCard(name: String)
+{
+    Text("Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard()
+{
+    MessageCard(name = "Android")
 }
